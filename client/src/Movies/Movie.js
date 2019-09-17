bac755bcf5d633c9808d343957f580024ac535e0
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-17 13:10:41 +0100

    deleted comments

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,28 +31,9 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  // const { title, director, metascore, stars } = movie;
+
   return (
     <MovieCard title = {movie.title} director = {movie.director} metascore = {movie.metascore} stars = {movie.stars}/>
-    // <div className="save-wrapper">
-    //   <div className="movie-card">
-    //     <h2>{title}</h2>
-    //     <div className="movie-director">
-    //       Director: <em>{director}</em>
-    //     </div>
-    //     <div className="movie-metascore">
-    //       Metascore: <strong>{metascore}</strong>
-    //     </div>
-    //     <h3>Actors</h3>
-
-    //     {stars.map(star => (
-    //       <div key={star} className="movie-star">
-    //         {star}
-    //       </div>
-    //     ))}
-    //   </div>
-    //   <div className="save-button">Save</div>
-    // </div>
   );
 }
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -34,25 +34,9 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <MovieCard title = {movie.title} director = {movie.director} metascore = {movie.metascore} stars = {movie.stars}/>
-    // <div className="movie-card">
-    //   <h2>{title}</h2>
-    //   <div className="movie-director">
-    //     Director: <em>{director}</em>
-    //   </div>
-    //   <div className="movie-metascore">
-    //     Metascore: <strong>{metascore}</strong>
-    //   </div>
-    //   <h3>Actors</h3>
-
-    //   {stars.map(star => (
-    //     <div key={star} className="movie-star">
-    //       {star}
-    //     </div>
-    //   ))}
-    // </div>
   );
 }
 
